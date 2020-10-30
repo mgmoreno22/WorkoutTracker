b3f523da2cdbf8c8e1a5db64be2b48180a6dc549
Michael Moreno <mmoreno2222@gmail.com>
2020-10-30 15:41:55 -0700

    Page navigation fixed

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-app.use(require("./routes/htmlRoutes.js"))
-app.use(require("./routes/apiRoutes.js"));
+// app.use(require("./routes/htmlRoutes.js"))
+// app.use(require("./routes/apiRoutes.js"));
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
